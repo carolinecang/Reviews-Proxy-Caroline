e47e85a068284ba68a3b27cab1b0e1905233b8b4
Caroline Cange <carolinecang@gmail.com>
2019-03-12 11:13:32 -0700

    fixed some of the proxy server

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const cors = require('cors');
 const parser = require('body-parser');
 const axios = require('axios');
-let id = null;
+let id = 123;
 
 const app = express();
-const port = 3005;
+const port = 3004;
 
 app.use(parser.json());
 app.use(parser.urlencoded({extended:true}));
@@ -20,72 +20,67 @@ app.use(parser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.post('/randomid', (req, res) => {
-    const {randomId} = req.body;
-    id = randomId;
-    console.log('proxy id', id)
-})
+// app.post('/randomid', (req, res) => {
+//     const {randomId} = req.body;
+//     id = randomId;
+//     console.log('proxy id', id)
+// })
 
-app.get('/suggestions', (req, res) => {
-    axios
-        .get('http://18.216.95.88:3004/suggestions', {
-            params: {
-                id: id
-            }
-        })
-        .then(({ data }) => res.send(JSON.stringify(data)))
-        .catch(err => res.send(JSON.stringify(err)))
-})
+// app.get('/suggestions', (req, res) => {
+//     axios
+//         .get('http://18.216.95.88:3004/suggestions', {
+//             params: {
+//                 id: id
+//             }
+//         })
+//         .then(({ data }) => res.send(JSON.stringify(data)))
+//         .catch(err => res.send(JSON.stringify(err)))
+// })
 
-app.get('/search/:keyword', (req, res) => {
-    let keyword = req.params.keyword;
-    axios
-        .get(`http://localhost:3001/search/${keyword}`)
-        .then(({data}) => res.send(JSON.stringify(data)))
-        .catch(err => console.log(err))
-})
+// app.get('/search/:keyword', (req, res) => {
+//     let keyword = req.params.keyword;
+//     axios
+//         .get(`http://localhost:3001/search/${keyword}`)
+//         .then(({data}) => res.send(JSON.stringify(data)))
+//         .catch(err => console.log(err))
+// })
 
-app.get('/abibas/product', (req, res) => {
-    axios
-        .get('http://3.86.105.203:3002/abibas/product', {
-            params: {
-                id: id
-            }
-        })
-        .then(({ data }) => res.send((data)))
-        .catch(err => res.send(err))
-})
+// app.get('/abibas/product', (req, res) => {
+//     axios
+//         .get('http://3.86.105.203:3002/abibas/product', {
+//             params: {
+//                 id: id
+//             }
+//         })
+//         .then(({ data }) => res.send((data)))
+//         .catch(err => res.send(err))
+// })
 
-app.get('/abibas/color', (req, res) => {
-    let colorId = req.query.id;
-    axios
-        .get('http://3.86.105.203:3002/abibas/product', {
-            params: {
-                id: colorId
-            }
-        })
-        .then(({ data }) => res.send((data)))
-        .catch(err => res.send(err))
-})
+// app.get('/abibas/color', (req, res) => {
+//     let colorId = req.query.id;
+//     axios
+//         .get('http://3.86.105.203:3002/abibas/product', {
+//             params: {
+//                 id: colorId
+//             }
+//         })
+//         .then(({ data }) => res.send((data)))
+//         .catch(err => res.send(err))
+// })
 
 app.get('/reviews', (req, res) => {
+    // const {id} = req.params
+    // console.log(id)
+    console.log('in proxy server get')
     axios
-        .get('http://18.191.191.154:3003/reviews', {
-            params: {
-                id: id
-            }
-        })
-        .then(({ data }) => res.send(JSON.stringify(data)))
+        .get(`https://18.218.95.162:3005/reviews/${id}`)
+        .then(({ data }) => console.log(JSON.stringify(data)))
         .catch(err => res.send(err))
 })
 
 app.get('/reviews/stats', (req, res) => {
     axios
-        .get('http://18.191.191.154:3003/reviews/stats', {
-            params: {
-                id: id
-            }
-        })
+        .get(`https://18.218.95.162:3005/reviews/stats/${id}`)
         .then(({ data }) => res.send(JSON.stringify(data)))
         .catch(err => res.send(err))
 })
